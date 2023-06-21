e0a3e30b6e07ab27c21f21cd40bd4952257e9000
Synchromatic <synchromatic.github@gmail.com>
2023-06-21 03:48:40 +0300

    update

--- js/script.js
+++ js/script.js
@@ -17,6 +17,51 @@ window.addEventListener ('scroll', sync, false);
 document.addEventListener ('DOMContentLoaded', adjustSidebarContentHeight);
 window.addEventListener ('resize', adjustSidebarContentHeight, false);
 
+document.addEventListener ('touchstart', touchHandler, true);
+document.addEventListener ('touchmove', touchHandler, true);
+document.addEventListener ('touchend', touchHandler, true);
+document.addEventListener ('touchcancel', touchHandler, true);
+
+function touchHandler (event) {
+  console.log (event);
+  var touches = event.changedTouches, first = touches[0], type = '';
+  switch (event.type) {
+    case 'touchstart':
+      type = 'mousedown';
+      break;
+    case 'touchmove':
+      type = 'mousemove';
+      break;
+    case 'touchend':
+      type = 'mouseup';
+      break;
+    default:
+      return;
+  }
+
+  var simulatedEvent = document.createEvent ('MouseEvent');
+  simulatedEvent.initMouseEvent (
+    type,
+    true,
+    true,
+    window,
+    1,
+    first.screenX,
+    first.screenY,
+    first.clientX,
+    first.clientY,
+    false,
+    false,
+    false,
+    false,
+    0 /*left*/,
+    null
+  );
+
+  first.target.dispatchEvent (simulatedEvent);
+  event.preventDefault ();
+}
+
 document
   .querySelector ('.sidebar-content')
   .addEventListener ('scroll', function () {
