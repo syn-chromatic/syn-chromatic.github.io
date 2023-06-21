8b2928df290b6e598a1d484a333ff2054139f689
Synchromatic <synchromatic.github@gmail.com>
2023-06-21 03:01:53 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,156 +1,175 @@
-var toc = document.querySelector( '.table-of-contents' );
-var tocPath = document.querySelector( '.toc-marker path' );
+var toc = document.querySelector ('.table-of-contents');
+var tocPath = document.querySelector ('.toc-marker path');
 var tocItems;
 
 // Factor of screen size that the element must cross
 // before it's considered visible
-var TOP_MARGIN = 0.1,
-    BOTTOM_MARGIN = 0.2;
-
+var TOP_MARGIN = 0.1;
+var BOTTOM_MARGIN = 0.2;
 var pathLength;
+var lastPathStart, lastPathEnd;
 
-var lastPathStart,
-		lastPathEnd;
+document.addEventListener ('DOMContentLoaded', drawPath);
+document.addEventListener ('DOMContentLoaded', sync);
+window.addEventListener ('resize', drawPathEvent, false);
+window.addEventListener ('scroll', sync, false);
 
-window.addEventListener( 'resize', drawPath, false );
-window.addEventListener( 'scroll', sync, false );
-document.addEventListener("DOMContentLoaded", adjustSidebarContentHeight);
-window.addEventListener("resize", adjustSidebarContentHeight);
+document.addEventListener ('DOMContentLoaded', adjustSidebarContentHeight);
+window.addEventListener ('resize', adjustSidebarContentHeight, false);
 
+document
+  .querySelector ('.sidebar-content')
+  .addEventListener ('scroll', function () {
+    var yDistance = -this.scrollTop + 12;
+    var tocMarker = document.querySelector ('.toc-marker path');
+    var tocMarkerTransition = tocMarker.style.transition;
 
-drawPath();
+    tocMarker.style.transition = 'none';
+    tocMarker.style.transform = 'translateY(' + yDistance + 'px)';
 
-document.querySelector('.sidebar-content').addEventListener('scroll', function() {
-    var scrollTop = this.scrollTop;
-    var tocMarker = document.querySelector('.toc-marker');
-    tocMarker.style.transform = 'translateY(' + (-scrollTop) + 'px)';
-});
+    var transitionEndEvents = [
+      'transitionend',
+      'webkitTransitionEnd',
+      'oTransitionEnd',
+      'otransitionend',
+      'MSTransitionEnd',
+    ];
 
+    var transitionRevert = function () {
+      tocMarker.style.transition = tocMarkerTransition;
+    };
 
-function adjustSidebarContentHeight() {
-    var sidebar = document.querySelector('.sidebar');
-    var sidebarContent = document.querySelector('.sidebar-content');
-
-    if (sidebar && sidebarContent) {
-        // Get the height, padding, and border
-        var sidebarHeight = parseFloat(getComputedStyle(sidebar).height);
-        var paddingTop = parseFloat(getComputedStyle(sidebar).paddingTop);
-        var paddingBottom = parseFloat(getComputedStyle(sidebar).paddingBottom);
-        var borderTop = parseFloat(getComputedStyle(sidebar).borderTopWidth);
-        var borderBottom = parseFloat(getComputedStyle(sidebar).borderBottomWidth);
-
-        // Calculate the available height within the sidebar
-        var availableHeight = sidebarHeight - paddingTop - paddingBottom - borderTop - borderBottom;
-
-        // Set the max-height of the sidebar-content
-        sidebarContent.style.maxHeight = availableHeight + 'px';
+    for (var i = 0; i < transitionEndEvents.length; i++) {
+      tocMarker.addEventListener (transitionEndEvents[i], transitionRevert);
     }
+  });
+
+function adjustSidebarContentHeight () {
+  var sidebar = document.querySelector ('.sidebar');
+  var sidebarContent = document.querySelector ('.sidebar-content');
+
+  if (sidebar && sidebarContent) {
+    // Get the height, padding, and border
+    var sidebarHeight = parseFloat (getComputedStyle (sidebar).height);
+    var paddingTop = parseFloat (getComputedStyle (sidebar).paddingTop);
+    var paddingBottom = parseFloat (getComputedStyle (sidebar).paddingBottom);
+    var borderTop = parseFloat (getComputedStyle (sidebar).borderTopWidth);
+    var borderBottom = parseFloat (
+      getComputedStyle (sidebar).borderBottomWidth
+    );
+
+    // Calculate the available height within the sidebar
+    var availableHeight =
+      sidebarHeight - paddingTop - paddingBottom - borderTop - borderBottom;
+
+    // Set the max-height of the sidebar-content
+    sidebarContent.style.maxHeight = availableHeight + 'px';
+  }
 }
 
+function drawPathEvent () {
+  var sidebar = document.querySelector ('.sidebar');
+  var transitionEndEvents = [
+    'transitionend',
+    'webkitTransitionEnd',
+    'oTransitionEnd',
+    'otransitionend',
+    'MSTransitionEnd',
+  ];
 
-function drawPath() {
+  for (var i = 0; i < transitionEndEvents.length; i++) {
+    sidebar.addEventListener (transitionEndEvents[i], drawPath);
+  }
+}
 
-  tocItems = [].slice.call( toc.querySelectorAll( 'li' ) );
+function drawPath () {
+  tocItems = [].slice.call (toc.querySelectorAll ('li'));
 
   // Cache element references and measurements
-  tocItems = tocItems.map( function( item ) {
-    var anchor = item.querySelector( 'a' );
-    var target = document.getElementById( anchor.getAttribute( 'href' ).slice( 1 ) );
+  tocItems = tocItems.map (function (item) {
+    var anchor = item.querySelector ('a');
+    var target = document.getElementById (
+      anchor.getAttribute ('href').slice (1)
+    );
 
     return {
       listItem: item,
       anchor: anchor,
-      target: target
+      target: target,
     };
-  } );
+  });
 
   // Remove missing targets
-  tocItems = tocItems.filter( function( item ) {
+  tocItems = tocItems.filter (function (item) {
     return !!item.target;
-  } );
+  });
 
   var path = [];
   var pathIndent;
 
-  tocItems.forEach( function( item, i ) {
-
+  tocItems.forEach (function (item, i) {
     var x = item.anchor.offsetLeft - 5,
-        y = item.anchor.offsetTop,
-        height = item.anchor.offsetHeight;
+      y = item.anchor.offsetTop,
+      height = item.anchor.offsetHeight;
 
-    if( i === 0 ) {
-      path.push( 'M', x, y, 'L', x, y + height );
+    if (i === 0) {
+      path.push ('M', x, y, 'L', x, y + height);
       item.pathStart = 0;
-    }
-    else {
+    } else {
       // Draw an additional line when there's a change in
       // indent levels
-      if( pathIndent !== x ) path.push( 'L', pathIndent, y );
+      if (pathIndent !== x) path.push ('L', pathIndent, y);
 
-      path.push( 'L', x, y );
+      path.push ('L', x, y);
 
       // Set the current path so that we can measure it
-      tocPath.setAttribute( 'd', path.join( ' ' ) );
-      item.pathStart = tocPath.getTotalLength() || 0;
+      tocPath.setAttribute ('d', path.join (' '));
+      item.pathStart = tocPath.getTotalLength () || 0;
 
-      path.push( 'L', x, y + height );
+      path.push ('L', x, y + height);
     }
-
     pathIndent = x;
-
-    tocPath.setAttribute( 'd', path.join( ' ' ) );
-    item.pathEnd = tocPath.getTotalLength();
-
-  } );
-
-  pathLength = tocPath.getTotalLength();
-  sync();
-
+    tocPath.setAttribute ('d', path.join (' '));
+    item.pathEnd = tocPath.getTotalLength ();
+  });
+  pathLength = tocPath.getTotalLength ();
 }
 
-function sync() {
-
+function sync () {
   var windowHeight = window.innerHeight;
-
-  var pathStart = pathLength,
-      pathEnd = 0;
-
+  var pathStart = pathLength, pathEnd = 0;
   var visibleItems = 0;
 
-  tocItems.forEach( function( item ) {
+  tocItems.forEach (function (item) {
+    var targetBounds = item.target.getBoundingClientRect ();
 
-    var targetBounds = item.target.getBoundingClientRect();
-
-    if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
-      pathStart = Math.min( item.pathStart, pathStart );
-      pathEnd = Math.max( item.pathEnd, pathEnd );
+    if (
+      targetBounds.bottom > windowHeight * TOP_MARGIN &&
+      targetBounds.top < windowHeight * (1 - BOTTOM_MARGIN)
+    ) {
+      pathStart = Math.min (item.pathStart, pathStart);
+      pathEnd = Math.max (item.pathEnd, pathEnd);
 
       visibleItems += 1;
 
-      item.listItem.classList.add( 'visible' );
+      item.listItem.classList.add ('visible');
+    } else {
+      item.listItem.classList.remove ('visible');
     }
-    else {
-      item.listItem.classList.remove( 'visible' );
-    }
-
-  } );
+  });
 
   // Specify the visible path or hide the path altogether
   // if there are no visible items
-  if( visibleItems > 0 && pathStart < pathEnd ) {
-    if( pathStart !== lastPathStart || pathEnd !== lastPathEnd ) {
-      tocPath.setAttribute( 'stroke-dashoffset', '1' );
-      tocPath.setAttribute( 'stroke-dasharray', '1, '+ pathStart +', '+ ( pathEnd - pathStart ) +', ' + pathLength );
-      tocPath.setAttribute( 'opacity', 1 );
+  if (visibleItems > 0 && pathStart < pathEnd) {
+    if (pathStart !== lastPathStart || pathEnd !== lastPathEnd) {
+      tocPath.setAttribute ('stroke-dashoffset', '1');
+      tocPath.setAttribute (
+        'stroke-dasharray',
+        '1, ' + pathStart + ', ' + (pathEnd - pathStart) + ', ' + pathLength
+      );
+      tocPath.setAttribute ('opacity', 1);
     }
+  } else {
+    tocPath.setAttribute ('opacity', 0);
   }
-  else {
-    tocPath.setAttribute( 'opacity', 0 );
-  }
-
-  lastPathStart = pathStart;
-  lastPathEnd = pathEnd;
-
 }
-
-
